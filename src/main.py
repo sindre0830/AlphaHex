e387c7cc9fc34bfd8e3362804684ea7bbe4a477a
Torjus Kallekleiv <torjus.kallekleiv@gmail.com>
2023-03-21 16:31:18 +0100

    play with AI

--- src/main.py
+++ src/main.py
@@ -4,36 +4,71 @@ from mcts import *
 from anet import *
 import numpy as np
 
-def main():
-    print("Starting AlphaHex...")
-    # Define ANET parameters
-    input_size = 3  # The number of piles in the Nim game
-    hidden_size = 64  # The number of neurons in the hidden layer
-    output_size = 6  # The number of unique actions (assuming a maximum of 2 stones per pile)
-
-    print("Ready to initialize ANET...")
-    # Initialize the ANET
-    anet = ANET(input_size, hidden_size, output_size)
-
-    # Define the initial state for the Nim game
-    initial_piles = [3, 4, 5]
-    nim_game_manager = Nim(initial_piles)
+class Main:
+    def __init__(self):
+        print("Starting AlphaHex...")
+        self.initial_piles = [3, 4, 5]  
+        self.game_manager = Nim(self.initial_piles)
+        self.input_size = 3  # The number of piles in the Nim game
+        self.hidden_size = 64  # The number of neurons in the hidden layer
+        self.output_size = 6  # The number of unique actions (assuming a maximum of 2 stones per pile)
+        self.max_games = 1000
+        self.max_game_variations = 100
+        self.exploration_constant = 1
+        print("Ready to initialize ANET...")
+        self.anet = ANET(input_size=2 * len(self.game_manager.initial_state), hidden_size=self.hidden_size, output_size=2 * len(self.game_manager.initial_state))
+        print("Ready to initialize MCTS...")
+        self.mcts = MCTS(self.game_manager, self.max_games, self.max_game_variations, self.anet, exploration_constant=self.exploration_constant)
 
     # Define MCTS parameters
     max_games = 10
     max_game_variations = 20
     exploration_constant = 1.0
 
-    print("Ready to initialize MCTS...")
-    # Initialize the MCTS
-    mcts = MCTS(game_manager=nim_game_manager, max_games=max_games, max_game_variations=max_game_variations, anet=anet, exploration_constant=exploration_constant)
 
-    # Run the MCTS algorithm
-    mcts.search()
+    def play(self):
+        current_state = self.game_manager.initial_state
+        current_player = 0  # 0 for AI, 1 for human
 
-    # Get the best move
-    best_move = mcts.get_best_move()
-    print(f"Best move: {best_move}")
+        while not self.game_manager.is_terminal(current_state):
+            print(f"Current state: {current_state}")
+            self.print_board(current_state)
+            if current_player == 0:
+                # AI's turn
+                print("AI's turn")
+                self.mcts.sear2ch(current_state)
+                best_move = self.mcts.get_best_move()
+                print(f"AI's move: {best_move}")
+            else:
+                # Human's turn
+                print("Your turn")
+                legal_actions = self.game_manager.get_legal_actions(current_state)
+                print(f"Legal actions: {legal_actions}")
+                best_move = self.get_human_move(legal_actions)
+
+            current_state = self.game_manager.next_state(current_state, best_move)
+            current_player = 1 - current_player
+
+        winner = "AI" if current_player == 1 else "You"
+        print(f"Game over. {winner} won!")
+
+    def get_human_move(self, legal_actions: list[tuple[int, int]]) -> tuple[int, int]:
+        while True:
+            pile_idx = int(input("Enter the pile index: "))
+            count = int(input("Enter the number of stones to take: "))
+            move = (pile_idx, count)
+            if move in legal_actions:
+                return move
+            else:
+                print("Invalid move. Please enter a valid move.")
+                
+    def print_board(self, state: list[int]) -> None:
+        print("\nCurrent board:")
+        for i, pile in enumerate(state):
+            print(f"Pile {i}: {'*' * pile}")
+        print()
+
 
 if __name__ == "__main__":
-    main()
+    main = Main()
+    main.play()
