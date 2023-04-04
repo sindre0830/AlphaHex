a4241e531c54092c86c8bd28e3df47045b1606b2
sindre0830 <sindre0830@gmail.com>
2023-04-04 08:01:33 +0200

    Initial commit of main.py

---
+++ source/main.py
@@ -0,0 +1,13 @@
+# external libraries
+import sys
+
+
+# Main program.
+def main():
+    args = sys.argv[1:]
+    print(args)
+
+
+# branch if program is run through 'python main.py'
+if __name__ == "__main__":
+    main()
