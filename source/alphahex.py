750cf50aa165bcfc09cb08cd6d19496afb3ce86d
sindre0830 <sindre0830@gmail.com>
2023-04-04 11:21:41 +0200

    Initial commit of Hex game manager

--- source/alphahex.py
+++ source/alphahex.py
@@ -4,22 +4,27 @@ from functionality import (
 )
 from rbuf import RBUF
 from anet import ANET
+from game_manager.hex import Hex
 
 
 class AlphaHex:
     def __init__(self, working_directory_path = ""):
         self.working_directory_path = working_directory_path
-        self.rbuf = RBUF()
-        self.anet = ANET()
         # load coniguration
         configuration = parse_json(directory_path=self.working_directory_path, file_name="configuration")
-        self.save_interval = configuration["save_interval"]
-        self.actual_games_size = configuration["actual_games_size"]
-        self.game_board_size = configuration["game_board_size"]
-        self.search_games_size = configuration["search_games_size"]
+        self.save_interval: int = configuration["save_interval"]
+        self.actual_games_size: int = configuration["actual_games_size"]
+        self.game_board_size: int = configuration["game_board_size"]
+        self.search_games_size: int = configuration["search_games_size"]
+        # init objects
+        self.rbuf = RBUF()
+        self.anet = ANET()
+        self.game_manager = Hex(self.game_board_size)
 
     def run(self):
         self.rbuf.clear()
         self.anet.initialize_model()
         for actual_game in range(self.actual_games_size):
-            pass
+            print(f"Actual game {(actual_game + 1):>{len(str(self.actual_games_size))}}/{self.actual_games_size}")
+            self.game_manager.initialize_empty_board()
+            
---
+++ source/game_manager/hex.py
@@ -0,0 +1,82 @@
+# external libraries
+from termcolor import colored 
+
+
+class Hex:
+    def __init__(self, board_size: int):
+        self.board_size = board_size
+        self.board = []
+    
+    def initialize_empty_board(self):
+        self.board = self.initial_state()
+
+    def initial_state(self) -> list[list[int]]:
+        return [[0 for _ in range(self.board_size)] for _ in range(self.board_size)]
+
+    def play_move(self, move, player):
+        if self.is_valid_action(self.board, move):
+            self.board = self.next_state(self.board, move, player)
+        else:
+            raise ValueError("Invalid move")
+
+    def is_terminal(self, state: list[list[int]]) -> bool:
+        def dfs(player, x, y, path):
+            if (player == 1 and y == len(state) - 1) or (player == 2 and x == len(state) - 1):
+                path.append((x, y))
+                return path
+
+            visited.add((x, y))
+            for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, 1), (1, -1)]:
+                nx, ny = x + dx, y + dy
+                if (0 <= nx < len(state) and 0 <= ny < len(state)
+                        and state[ny][nx] == player and (nx, ny) not in visited):
+                    new_path = dfs(player, nx, ny, path + [(x, y)])
+                    if new_path:
+                        return new_path
+            return False
+
+        for player in [1, 2]:
+            for i in range(len(state)):
+                visited = set()
+                start_x, start_y = (0, i) if player == 2 else (i, 0)
+                if state[start_y][start_x] == player:
+                    winning_path = dfs(player, start_x, start_y, [])
+                    if winning_path:
+                        return True
+
+        for row in state:
+            if 0 in row:
+                return False
+        return True
+    
+    def get_legal_actions(self, state: list[list[int]]) -> list[tuple[int, int]]:
+        legal_actions = [(x, y) for x in range(self.board_size) for y in range(self.board_size) if state[y][x] == 0]
+        return legal_actions
+
+    def next_state(self, state: list[list[int]], action: tuple[int, int], player: str) -> list[list[int]]:
+        x, y = action
+        next_state = [row.copy() for row in state]
+        next_state[y][x] = player
+        return next_state
+
+    def is_valid_action(self, state: list[list[int]], action: tuple[int, int]) -> bool:
+        x, y = action
+        return 0 <= x < self.board_size and 0 <= y < self.board_size and state[y][x] == 0
+    
+    def print_state(self, state: list[list[int]], winning_path=None):
+        """
+        Method to print the current game state
+        """
+        if winning_path is None:
+            winning_path = []
+
+        winning_path_set = set(winning_path)
+        for i, row in enumerate(state):
+            print(' ' * i, end='')
+            colored_row = []
+            for j, cell in enumerate(row):
+                if (j, i) in winning_path_set:
+                    colored_row.append(colored(str(cell), "red"))
+                else:
+                    colored_row.append(str(cell))
+            print(' '.join(colored_row))
