d9260e9eca2a64b7ee2ce017d71cc5926fc31c38
sindre0830 <sindre0830@gmail.com>
2023-04-05 23:37:35 +0200

    Add prints

--- source/alphahex.py
+++ source/alphahex.py
@@ -16,6 +16,8 @@ from game_manager.hex import (
 class AlphaHex:
     def __init__(self, working_directory_path = ""):
         self.working_directory_path = working_directory_path
+        self.simulated_games_count = 0
+        self.game_moves_count = 0
         # load coniguration
         configuration = parse_json(directory_path=self.working_directory_path, file_name="configuration")
         self.save_interval: int = configuration["save_interval"]
@@ -37,8 +39,14 @@ class AlphaHex:
             self.game_manager.initialize_empty_board()
             self.mct.set_root_node(self.game_manager.board, self.game_manager.player)
             while not self.game_manager.terminal():
+                self.increment_game_moves_count()
                 self.mct.update_game_board(self.mct.root_node.board)
                 for search_game in range(self.search_games_size):
+                    print(
+                        f"\tSimulated game {(search_game + 1):>{len(str(self.search_games_size))}}/{self.search_games_size}, Total simulated games: {self.increment_simulated_games_count()}, Total moves: {self.game_moves_count}",
+                        end="\r",
+                        flush=True
+                    )
                     leaf = self.mct.tree_search()
                     self.mct.node_expansion(leaf)
                     score = self.mct.leaf_evaluation(self.anet, leaf)
@@ -48,6 +56,19 @@ class AlphaHex:
                 actual_move = action_from_visit_distribution(visit_distribution, self.game_board_size)
                 self.game_manager.play_move(actual_move)
                 self.mct.set_root_node(self.game_manager.board, self.game_manager.player)
+            self.reset_counts()
             self.anet.train(self.rbuf.get_mini_batch(self.mini_batch_size))
             if actual_game % self.save_interval == 0:
                 self.anet.save(actual_game)
+
+    def increment_simulated_games_count(self) -> int:
+        self.simulated_games_count += 1
+        return self.simulated_games_count
+
+    def increment_game_moves_count(self):
+        self.game_moves_count += 1
+    
+    def reset_counts(self):
+        self.simulated_games_count = 0
+        self.game_moves_count = 0
+        print()
