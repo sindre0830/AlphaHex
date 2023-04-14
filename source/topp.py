f2339fa1b9252e32215deabef8ecc65b63cb27d6
Sindre Eiklid <sindre0830@gmail.com>
2023-04-14 23:56:35 +0200

    Add support for multi alphahex input to TOPP

--- source/topp.py
+++ source/topp.py
@@ -16,37 +16,44 @@ import random
 
 
 class TOPP:
-    def __init__(self, device: torch.cuda.device, device_type: str, alphahex_directory_name: str):
-        self.working_directory_path = f"{DATA_PATH}/{alphahex_directory_name}"
-        # load config
-        self.configuration = parse_json(self.working_directory_path + "/", "configuration")
-        self.board_size = self.configuration["game_board_size"]
-        # load models
-        model_file_paths = glob.glob(f"{self.working_directory_path}/*.pt")
+    def __init__(self, device: torch.cuda.device, device_type: str, alphahex_directory_names: str):
         models: list[ANET] = []
-        self.model_iterations: list[int] = []
-        for model_file_path in model_file_paths:
-            # get model
-            anet = ANET(
-                device,
-                device_type,
-                self.board_size,
-                self.configuration["epochs"],
-                self.configuration["input_layer"],
-                self.configuration["hidden_layers"],
-                self.configuration["criterion"],
-                self.configuration["optimizer"]
-            )
-            anet.initialize_model(saved_model_path=model_file_path)
-            models.append(anet)
-            # get model iteration
-            filename = os.path.basename(model_file_path)
-            name, _ = os.path.splitext(filename)
-            self.model_iterations.append(int(name.split('-')[-1]))
+        self.model_iterations: list[str] = []
+        self.board_size = None
+        # remove duplicates
+        alphahex_directory_names = list(dict.fromkeys(alphahex_directory_names))
+        for alphahex_index in range(len(alphahex_directory_names)):
+            working_directory_path = f"{DATA_PATH}/{alphahex_directory_names[alphahex_index]}"
+            # load config
+            configuration = parse_json(working_directory_path + "/", "configuration")
+            if self.board_size is None:
+                self.board_size = configuration["game_board_size"]
+            elif self.board_size != configuration["game_board_size"]:
+                raise Exception("The board size needs to be the same for all models.")
+            # load models
+            model_file_paths = glob.glob(f"{working_directory_path}/*.pt")
+            for model_file_path in model_file_paths:
+                # get model
+                anet = ANET(
+                    device,
+                    device_type,
+                    self.board_size,
+                    configuration["epochs"],
+                    configuration["input_layer"],
+                    configuration["hidden_layers"],
+                    configuration["criterion"],
+                    configuration["optimizer"]
+                )
+                anet.initialize_model(saved_model_path=model_file_path)
+                models.append(anet)
+                # get model iteration
+                filename = os.path.basename(model_file_path)
+                name, _ = os.path.splitext(filename)
+                self.model_iterations.append(f"{alphahex_directory_names[alphahex_index]}_{name.split('-')[-1]}")
         # sort and store models and their iteration index together
         self.models = list(zip(models, self.model_iterations))
         # results
-        self.total_score: dict[int, dict[int, int]] = {}
+        self.total_score: dict[str, dict[int, int]] = {}
         for (_, iteration) in self.models:
             self.total_score[iteration] = {}
             self.total_score[iteration]["Total"] = 0
@@ -55,10 +62,10 @@ class TOPP:
 
     def run(self):
         # prepare all possible pairings
-        pairings: list[tuple[tuple[ANET, int], tuple[ANET, int]]] = []
+        pairings: list[tuple[tuple[ANET, str], tuple[ANET, str]]] = []
         for pair in itertools.combinations(self.models, 2):
             pairings.append(pair)
-        opposite_pairings: list[tuple[tuple[ANET, int], tuple[ANET, int]]] = []
+        opposite_pairings: list[tuple[tuple[ANET, str], tuple[ANET, str]]] = []
         for (model_1, model_2) in pairings:
             opposite_pairings.append((model_2, model_1))
         pairings += opposite_pairings
